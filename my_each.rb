af9cfa63ad67901d69673343167a4dd64c70c15e
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-08 20:36:19 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
+words = ["why", "wont", "this", "work"]
+
+def my_each(words) # put argument(s) here
   # code here
-end
+  if block_given?
+  x = 0 
+  
+  while x < words.length 
+  yield(words[x])
+  x = x + 1 
+  end 
+    words
+  else
+   "Where's my block?"
+  end 
+end 
+
